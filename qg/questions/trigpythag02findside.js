ce38d28a29f00017c5ade71e5c20ce561a730e5b
mathsnz <jwills@mathsnz.com>
2016-02-11 21:33:48 +1300

    update

---
+++ qg/questions/trigpythag02findside.js
@@ -0,0 +1,3 @@
+
+function trigpythag02findside(){a=rand(0.5,7,0.1);c=rand(parseFloat(a),10,0.1);b="x";tri=randint(1,8);var answer=Math.sqrt(Math.pow(c,2)-Math.pow(a,2)).toPrecision(3);var question='<img width=300 src="http://www.jake4maths.com/generator/triangle'+tri+'.php?a='+a+'&b='+b+'&c='+c+'&ang1=&ang2="><br>x = '+numberanswer(answer);var description="Find the size of x. Note: Diagrams are not to scale. Give answer to at least 3 significant figures.";var array={question:question,answer:answer,description:description}
+return array;}
